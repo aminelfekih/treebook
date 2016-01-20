2f8cd0ed75e9130c5945d698f3e968bc1e45a0d3
aminelfekih <elfekihamine@gmail.com>
2016-01-20 11:32:28 +0100

    Install devise , signup and signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users , controllers: {
+    confirmations: "users/confirmations",
+    passwords: "users/passwords",
+    registrations: 'users/registrations',
+    sessions: "users/sessions",
+    unlocks: "users/unlocks" 
+  }
   resources :statuses
+  root to: "statuses#index"
+
+  #root '/home' => "application#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -15,7 +25,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  # Example resource route with options:
+  # Example resource route with options:    
   #   resources :products do
   #     member do
   #       get 'short'
